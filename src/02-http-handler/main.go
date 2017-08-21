b29a1d79852467502465b358703061516dc61d08
Maksim Kostromin <daggerok@gmail.com>
2017-08-21 23:39:24 +0300

    update 2nd project.

--- src/02-http-handler/main.go
+++ src/02-http-handler/main.go
@@ -1,10 +1,23 @@
 package main
 
-import "net/http"
+import (
+  "net/http"
+  "fmt"
+)
+
+// custom type
+type MyHandler struct {
+  greeting string
+}
+
+// implement a handler interface which is will call ServeHTTP
+func (mh MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte(fmt.Sprintf("%v world", mh.greeting)))
+}
 
 func main() {
-  http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-    writer.Write([]byte("hi!"))
-  })
-  http.ListenAndServe(":8080", nil)
+  // create instance of custom request handler
+  handler := &MyHandler{greeting: "ololo"}
+  http.Handle("/", handler)
+  http.ListenAndServe(":8080", handler)
 }
