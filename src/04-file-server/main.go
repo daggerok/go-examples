3bf31652814bff5f879f984957b5688195bf07f5
Maksim Kostromin <daggerok@gmail.com>
2017-08-22 00:42:41 +0300

    implement http-web-server

---
+++ src/04-file-server/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+  "net/http"
+  "os"
+  "log"
+  "io"
+  "strings"
+)
+
+func setContentType(writer http.ResponseWriter, contentType string) {
+  writer.Header().Add("Content-Type", contentType)
+}
+
+func main() {
+  http.HandleFunc("/", func(response http.ResponseWriter, request *http.Request) {
+    path := request.URL.Path
+    file, err := os.Open("src/04-file-server" + path)
+    if err != nil {
+      response.WriteHeader(http.StatusInternalServerError)
+      switch {
+      case strings.HasSuffix(path, ".html"):
+        setContentType(response, "text/html")
+      case strings.HasSuffix(path, ".css"):
+        setContentType(response, "text/css")
+      case strings.HasSuffix(path, ".js"):
+        setContentType(response, "application/javascript")
+      case strings.HasSuffix(path, ".png"):
+        setContentType(response, "image/png")
+      case strings.HasSuffix(path, ".ico"):
+        setContentType(response, "image/x-icon")
+      default:
+        setContentType(response, "text/plain")
+      }
+      log.Println(err)
+    }
+    defer file.Close()
+    io.Copy(response, file)
+  })
+  http.ListenAndServe(":8080", nil)
+}
